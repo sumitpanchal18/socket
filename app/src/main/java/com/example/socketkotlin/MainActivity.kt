bc41bba11cab44c84c6fc4d740e5fe7e2d4af169
Sumit <panchalsumite2logy@gmail.com>
2024-10-23 11:57:12 +0530

    project created

--- app/src/main/java/com/example/socketkotlin/MainActivity.kt
+++ app/src/main/java/com/example/socketkotlin/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.socketkotlin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.socketkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
